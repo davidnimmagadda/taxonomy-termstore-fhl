da201b09b6bcd8c93d22c09a2a00d061a582a863
David John Nimmagadda <danimmag@microsoft.com>
2019-07-24 12:44:50 +0530

    Added API to get terms

--- tools/createMockDb.js
+++ tools/createMockDb.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const path = require("path");
 const mockData = require("./mockData");
 
-const { terms, authors } = mockData;
-const data = JSON.stringify({ terms, authors });
+const { terms, authors, ts } = mockData;
+const data = JSON.stringify({ terms, authors, ts });
 const filepath = path.join(__dirname, "db.json");
 
 fs.writeFile(filepath, data, function(err) {
--- tools/mockData.js
+++ tools/mockData.js
@@ -84,9 +84,71 @@ const newTerm = {
   category: ""
 };
 
+const ts = [
+  {
+    id: "termstore",
+    path: "/termstore",
+    type: "folder",
+    isRoot: true,
+    children: ["/termstore:Group1", "/termstore:Group2"]
+  },
+  {
+    id: "termstore:Group1",
+    path: "/termstore:Group1",
+    type: "folder",
+    children: ["/termstore:Group1:TestTerm"]
+  },
+  {
+    id: "termstore:Group1:TestTerm",
+    path: "/termstore:Group1:TestTerm",
+    type: "folder",
+    children: ["/termstore:Group1:TestTerm:SubTerm"]
+  },
+  {
+    id: "termstore:Group1:TestTerm:SubTerm",
+    path: "/termstore:Group1:TestTerm:SubTerm",
+    type: "folder",
+    children: []
+  },
+  {
+    id: "termstore:Group2",
+    path: "/termstore:Group2",
+    type: "folder",
+    children: ["/termstore:Group2:TermSet1", "/termstore:Group2:TermSet2"]
+  },
+  {
+    id: "termstore:Group2:TermSet1",
+    path: "/termstore:Group2:TermSet1",
+    type: "folder",
+    children: [
+      "/termstore:Group2:TermSet1:Term1",
+      "/termstore:Group2:TermSet1:Term2"
+    ]
+  },
+  {
+    id: "termstore:Group2:TermSet1:Term1",
+    path: "/termstore:Group2:TermSet1:Term1",
+    type: "folder",
+    children: []
+  },
+  {
+    id: "termstore:Group2:TermSet1:Term2",
+    path: "/termstore:Group2:TermSet1:Term2",
+    type: "folder",
+    children: []
+  },
+  {
+    id: "termstore:Group2:TermSet2",
+    path: "/termstore:Group2:TermSet2",
+    type: "folder",
+    children: []
+  }
+];
+
 // Using CommonJS style export so we can consume via Node (without using Babel-node)
 module.exports = {
   newTerm,
   terms,
-  authors
+  authors,
+  ts
 };
