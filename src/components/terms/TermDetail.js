965ef192eed4600951efda930f108b8808868ab4
gayathrisns <gayathri6feb@gmail.com>
2019-07-24 14:20:50 +0530

    Commit with state addition

--- src/components/terms/TermDetail.js
+++ src/components/terms/TermDetail.js
@@ -5,17 +5,15 @@ import { loadTermDetails } from "../../redux/actions/termActions";
 
 function TermDetail({ term, ...props }) {
   useEffect(() => {
-    if (props.terms.length === 0) {
-      props
-        .loadTermDetails()
-        .catch(error => alert("Loading Terms Failed : " + error));
-    }
+    props
+      .loadTermDetails()
+      .catch(error => alert("Loading Terms Failed : " + error));
   }, []);
   return <div>Term {term}</div>;
 }
 TermDetail.propTypes = {
   term: PropTypes.string.isRequired,
-  terms: PropTypes.array.isRequired
+  loadTermDetails: PropTypes.func.isRequired
 };
 function mapStateToProps({ terms, activeApiCalls }) {
   return { terms, loading: activeApiCalls > 0 };
